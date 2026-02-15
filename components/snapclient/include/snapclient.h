ad8cb37f4c64ff63f394c24dbdd209b964b9272d
raul <raul11@freenet.de>
2026-02-15 19:40:41 +0100

    create snapclient component

---
+++ components/snapclient/include/snapclient.h
@@ -0,0 +1,14 @@
+#ifndef __SNAPCLIENT_H__
+#define __SNAPCLIENT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_snapcast(void (*set_volume)(int), void (*set_mute)(bool));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __SNAPCLIENT_H__
